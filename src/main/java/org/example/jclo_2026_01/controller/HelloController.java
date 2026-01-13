599abde7feb2effcb3667eebb430f6b3362ab2bf
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-01-13 19:56:41 +0300

    Забираем в класс значение из application.properties через @ConfigurationProperties

--- src/main/java/org/example/jclo_2026_01/controller/HelloController.java
+++ src/main/java/org/example/jclo_2026_01/controller/HelloController.java
@@ -1,12 +1,22 @@
 package org.example.jclo_2026_01.controller;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties("hello") // раздел в application.properties, из которого будем брать конфигурацию для этого файла
+                                  // при старте пройдемся по всем свойствам hello.* и заберем оттуда значения
+                                  // чтобы это сработало - нужен еще в классе сеттер
 public class HelloController {
+    private String from;
+
+    public void setFrom (String from) {
+        this.from = from;
+    }
+
     @GetMapping("/")
     private String hello () {
-        return "Hello from app";
+        return String.format("Hello from %s", from);
     }
 }
