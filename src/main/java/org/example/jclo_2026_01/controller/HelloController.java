af9c27b057facf7f36edb49537e7a0d4cca38c96
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-01-13 21:26:56 +0300

    Обработка POST запроса с автоматической десериализацией

--- src/main/java/org/example/jclo_2026_01/controller/HelloController.java
+++ src/main/java/org/example/jclo_2026_01/controller/HelloController.java
@@ -1,7 +1,10 @@
 package org.example.jclo_2026_01.controller;
 
+import org.example.jclo_2026_01.domain.Person;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +18,9 @@ public class HelloController {
         this.from = from;
     }
 
-    @GetMapping("/")
-    private String hello () {
-        return String.format("Hello from %s", from);
+    @PostMapping("/hello") // В теле запроса будем передавать в виде JSON Person
+    private String hello (@RequestBody Person guest) {
+        return String.format("Hello from %s to name %s age %d",
+                from, guest.getName(), guest.getAge());
     }
 }
---
+++ src/main/java/org/example/jclo_2026_01/domain/Person.java
@@ -0,0 +1,54 @@
+package org.example.jclo_2026_01.domain;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    // методы для работы Jackson, который дефолтный для сериализации
+    // - конструктор без параметров
+    // - геттеры и сеттеры для всех свойств
+    // это все можно не генерить в IDE, а указать специальные аннотации
+    public Person(){
+    }
+
+    public Person(String name, int age) { // заодно добавим конструктор со всеми аргументами
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
