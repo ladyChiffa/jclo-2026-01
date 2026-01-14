808c845eabcadb8356bc8bf37c70e4de72e25437
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-01-14 20:31:45 +0300

    Прямая обработка ошибки (@RequestMapping добавили, чтобы выделить часть API для ошибок)

--- src/main/java/org/example/jclo_2026_01/controller/BrokenController.java
+++ src/main/java/org/example/jclo_2026_01/controller/BrokenController.java
@@ -3,14 +3,26 @@ package org.example.jclo_2026_01.controller;
 import org.example.jclo_2026_01.domain.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("/error")
 public class BrokenController {
     @GetMapping("/error")
-    private String error () {
+    public String error () {
         System.out.println("logic");
         throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT, "ResponseStatusException");
+    }
+
+    @GetMapping("/{id}")
+    public Person getPersonById(@PathVariable("id") long id) {
+        try {
+            throw new IllegalArgumentException("IllegalArgumentException");
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ResponseStatusException", e);
+        }
     }
 }
