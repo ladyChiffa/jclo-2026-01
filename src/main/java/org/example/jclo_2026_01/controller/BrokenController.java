988b505e2fe6982d1b6edaf54895c1dcef785910
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-01-14 20:44:04 +0300

    Обработка с помощью обработчиков ExceptionHandlers

--- src/main/java/org/example/jclo_2026_01/controller/BrokenController.java
+++ src/main/java/org/example/jclo_2026_01/controller/BrokenController.java
@@ -1,11 +1,10 @@
 package org.example.jclo_2026_01.controller;
 
+import jdk.javadoc.doclet.Reporter;
 import org.example.jclo_2026_01.domain.Person;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -24,5 +23,20 @@ public class BrokenController {
         } catch (RuntimeException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ResponseStatusException", e);
         }
+    }
+
+    @GetMapping("/illegal")
+    public Person throwException() {
+        throw new IllegalArgumentException("IllegalArgumentException");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalHandler(IllegalArgumentException e) {
+        return new ResponseEntity<>("Exception in throwException method", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> reHandler() {
+        return new ResponseEntity<>("Exception in throwException method", HttpStatus.I_AM_A_TEAPOT);
     }
 }
